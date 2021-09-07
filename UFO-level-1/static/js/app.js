e9ba8bfdadd0074d4e54956d6f9020ce070671ce
ramses93 <ramirezelida11@yahoo.com>
2021-09-06 22:59:06 -0400

    Updated app.js
    Added functionality to Filter button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -17,7 +17,6 @@ tbody.html("");
 table.attr("class", "table table-striped");
 // Use d3 to append 1 cell per UFO sighting value 
 tableData.forEach(function(ufoSighting){
-    console.log(ufoSighting);
     var row =tbody.append("tr");
 
     Object.entries(ufoSighting).forEach(([key,value]) => {
@@ -28,3 +27,37 @@ tableData.forEach(function(ufoSighting){
 
 // get the Filter button
 var button = d3.select("#filter-btn");
+
+// add listener
+// In this listener all search and diplay for the table will be performed
+button.on("click", function() {
+	// Get date to search
+	var date = d3.select("#datetime");
+	// Get value 
+	var date_value = date.property("value");
+
+	// perform query
+	var data = tableData.filter(ufoSighting => (ufoSighting.datetime ===date_value));
+	
+	// Use D3 to select the table body
+	var tbody = d3.select("tbody");
+	// Use D3 to select the table
+	var table = d3.select("table");
+	//remove any children from the table to 
+	tbody.html("");
+	// Use D3 to set the table class to `table table-striped`
+	table.attr("class", "table table-striped");
+
+
+	if (date_value !== "")
+	{ // if the date is not empty
+		data.forEach(function(ufoSighting){
+			var row =tbody.append("tr"); // add a new tr element
+
+			Object.entries(ufoSighting).forEach(([key,value]) => {
+				var cell = row.append("td"); // add td element
+				cell.text(value);
+			});
+		});
+	}
+});
