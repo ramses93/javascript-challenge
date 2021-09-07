22f5589c98d8b2e9e8f462b4814d924d4b4d8230
ramses93 <ramirezelida11@yahoo.com>
2021-09-06 23:02:03 -0400

    Added UFO-level-2
    Added UFO-level-2 as a copy of UFO-level-1

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,80 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+// select the table body
+var tbody = d3.select("tbody");
+
+// select table
+var table = d3.select("table");
+
+
+// delete any object in table
+tbody.html("");
+
+// Use D3 to set the table class to `table table-striped`
+table.attr("class", "table table-striped");
+// Use d3 to append 1 cell per UFO sighting value 
+tableData.forEach(function(ufoSighting){
+    var row =tbody.append("tr");
+
+    Object.entries(ufoSighting).forEach(([key,value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// get the Filter button
+var button = d3.select("#filter-btn");
+
+// add listener
+// In this listener all search and diplay for the table will be performed
+button.on("click", function() {
+	// Get date to search
+	var date = d3.select("#datetime");
+	// Get value 
+	var date_value = date.property("value");
+
+	// perform query
+	var data = tableData.filter(ufoSighting => (ufoSighting.datetime ===date_value));
+	
+
+	// select the table body
+	var tbody = d3.select("tbody");
+
+	// select table
+	var table = d3.select("table");
+
+
+	// delete any object in table
+	tbody.html("");
+
+	// Use D3 to set the table class to `table table-striped`
+	table.attr("class", "table table-striped");
+
+
+	if (date_value !== "")
+	{ // if the date is not empty
+		data.forEach(function(ufoSighting){
+			var row =tbody.append("tr"); // add a new tr element
+
+			Object.entries(ufoSighting).forEach(([key,value]) => {
+				var cell = row.append("td"); // add td element
+				cell.text(value);
+			});
+		});
+	}
+	else // value is empty, so no date has been inputted
+	{
+		// append empty elements
+		tableData.forEach(function(ufoSighting){
+			var row =tbody.append("tr");
+		
+			Object.entries(ufoSighting).forEach(([key,value]) => {
+				var cell = row.append("td");
+				cell.text(value);
+			});
+		});
+	}
+});
